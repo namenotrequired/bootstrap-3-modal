0de77e40ffa3787dc26cb866db614d29a8ac810f
Bart Sturm <bart@tutormundi.com>
2020-10-24 16:10:29 -0300

    don't allowMultiple, but switch

--- main.js
+++ main.js
@@ -1,51 +1,46 @@
-var $soloModal = null // Used when allowMultiple = false.
+let $currentModal;
 
 // The public API.
 Modal = {
-	
-	allowMultiple: false,
-	
-	show: function(templateName, data, options){
-		
-		if($soloModal == null || this.allowMultiple){
-			
-			var parentNode = document.body
-			
-			var view = Blaze.renderWithData(Template[templateName], data, parentNode)
-			
-			var domRange = view._domrange // TODO: Don't violate against the public API.
-			
-			var $modal = domRange.$('.modal')
-			
-			$modal.on('shown.bs.modal', function(event){
-				$modal.find('[autofocus]').focus()
-			})
-			
-			$modal.on('hidden.bs.modal', function(event){
-				Blaze.remove(view)
-				$soloModal = null
-			})
-			
-			$soloModal = $modal
-			
-			$modal.modal(options ? options : {})
-			
-		}
-		
-	},
-	
-	hide: function(/* optional */ template){
-		
-		if(template instanceof Blaze.TemplateInstance){
-			
-			template.$('.modal').modal('hide')
-			
-		}else if($soloModal != null){
-			
-			$soloModal.modal('hide')
-			
-		}
-		
-	}
-	
-}
+  show: (templateName, data, options = {}) => {
+    // If there is a shown modal, hide it
+    if ($currentModal) {
+      Modal.hide();
+    };
+
+    // Show the modal
+    var parentNode = document.body;
+
+    var view = Blaze.renderWithData(Template[templateName], data, parentNode);
+
+    var domRange = view._domrange; // TODO: Don't violate against the public API.
+
+    var $modal = domRange.$('.modal');
+
+    $modal.on('shown.bs.modal', () => {
+      $modal.find('[autofocus]').focus();
+    });
+
+    $modal.on('hidden.bs.modal', () => {
+      Blaze.remove(view);
+    });
+
+    $currentModal = $modal;
+
+    $currentModal.name = templateName;
+
+    $modal.modal(options);
+  },
+
+  hide: () => {
+    if (!$currentModal) return;
+
+    const { name = '' } = $currentModal;
+
+    $currentModal.modal('hide');
+
+    $currentModal = null;
+
+    return name;
+  },
+};
--- package.js
+++ package.js
@@ -1,23 +1,21 @@
 Package.describe({
-	summary: "Simple usage of bootstrap 3 modals.",
-	version: "1.0.4",
-	name: "peppelg:bootstrap-3-modal",
-	git: "https://github.com/PeppeL-G/bootstrap-3-modal.git"
-})
+  summary: 'Simple usage of bootstrap 3 modals',
+  version: '2.0.0',
+  name: 'bartoftutormundi:bootstrap-3-modal',
+  git: 'https://github.com/namenotrequired/bootstrap-3-modal.git',
+});
 
-Package.onUse(function(api){
-	
-	api.versionsFrom('METEOR@1.0.3')
-	
-	api.use([
-		'templating',
-		'jquery'
-	], 'client')
-	
-	api.addFiles([
-		'main.js'
-	], 'client')
-	
-	api.export('Modal', 'client')
-	
-})
+Package.onUse((api) => {
+  api.versionsFrom('METEOR@1.0.3');
+
+  api.use([
+    'templating',
+    'jquery',
+  ], 'client');
+
+  api.addFiles([
+    'main.js',
+  ], 'client');
+
+  api.export('Modal', 'client');
+});
